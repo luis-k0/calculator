f03506d5da767895eab0c1b17b95c19038954590
luis-k0-janis <luisk.net@gmail.com>
2019-03-01 19:35:22 -0300

    changing app to a functional component but calculation doesn't work

--- src/component/App.js
+++ src/component/App.js
@@ -1,30 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
 import calculate from "../logic/calculate";
 import "./App.css";
 
-class App extends React.Component {
-  state = {
+// class App extends React.Component {
+const app = props => {
+  // state = {
+  //   total: null,
+  //   next: null,
+  //   operation: null,
+  // };
+  const [memo, setMemo] = useState({
     total: null,
     next: null,
     operation: null,
+  });
+
+  const handleClick = buttonName => {
+    // this.setState(calculate(this.state, buttonName));
+    setMemo(calculate(memo, buttonName));
   };
-  // constructor(props) {
-  //   super(props);
+
+  // render() {
+  return (
+    <div className="component-app">
+      <Display value={memo.next || memo.total || "0"} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
   // }
-
-  handleClick = buttonName => {
-    this.setState(calculate(this.state, buttonName));
-  };
-
-  render() {
-    return (
-      <div className="component-app">
-        <Display value={this.state.next || this.state.total || "0"} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </div>
-    );
-  }
-}
-export default App;
+};
+export default app;
